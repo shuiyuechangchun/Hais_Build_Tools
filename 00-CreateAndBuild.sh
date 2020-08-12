ba7adab1de592fc6989d4d6fe9715939e138b3be
韦海生 <hais1992@163.com>
2020-08-12 17:54:21 +0800

    路径

--- 00-CreateAndBuild.sh
+++ 00-CreateAndBuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SH_FILE=$(readlink -f "$0")			#脚本的文件
+BASE_PATH=$(dirname $SH_FILE)		#脚本的目录
 ROM_FILE=${1}									#ROM文件
 IS_ZIP=${2}										#是否极限压缩,1-9
 if [ ! -n "$ROM_FILE" ] ; then echo "请传入ROM路径";exit;fi;
@@ -20,9 +22,12 @@ cp -rf ./0* ./$ROM_BUILD_FILE/
 sed -i "s/ZIP_LEVEL=1/ZIP_LEVEL=${IS_ZIP}/" $ROM_BUILD_FILE/00-Bins/_init_hais_tools_
 echo ">开始0${IS_ZIP}级编译：${ROM_FILE}"
 
-sudo bash "${ROM_BUILD_FILE}/01-UnpackRom.sh" ${ROM_FILE}
-sudo bash "${ROM_BUILD_FILE}/02-StartHMO.sh" ${ROM_FILE}
-sudo bash "${ROM_BUILD_FILE}/03-BuildRom.sh" ${ROM_FILE}
+
+cd ./$ROM_BUILD_FILE
+sudo bash ./01-UnpackRom.sh ../../$ROM_FILE
+sudo bash ./02-StartHMO.sh ../../$ROM_FILE
+sudo bash ./03-BuildRom.sh ../../$ROM_FILE
+cd $BASE_PATH
 
 
 echo ">打包结束，正在上传文件"
